4f99527d2626bb75cfb72c3f2a1b2503de2b395d
Kerosz <andrei_chirila@outlook.com>
2020-12-12 15:16:59 +0200

    pushing latest

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
+import Branding from './branding';
+
+function hideOnScroll({ children }) {
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <CssBaseline />
+      <hideOnScroll>
+        <AppBar>
+          <Toolbar>
+            <Branding />
+          </Toolbar>
+        </AppBar>
+      </hideOnScroll>
+      <Toolbar />
+      <Container>
+        <Box my={2}>
+          {[...new Array(22)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+      </Container>
+    </>
+  );
 }
+
+hideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+};
